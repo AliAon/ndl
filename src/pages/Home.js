53b1424a87027d410092f2ebd43d8384e0bf81be
aon <aon094944@gmail.com>
2023-02-03 18:55:16 +0500

    home page updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,26 @@
 import { Fragment } from "react";
 import { Container } from "react-bootstrap";
 import Footer from "../common/Footer";
-import Header from "../common/Header"
+import Header from "../common/Header";
 import BookNow from "../components/Home/BookNow";
 import GetQuote from "../components/Home/GetQuote";
 import Hero from "../components/Home/Hero";
 const Home = () => {
   return (
     <Fragment>
-      <Header/>
+      <Header />
       <main>
         <Container>
           <Hero />
-          <BookNow/>
-          <GetQuote/>
+          <BookNow />
         </Container>
+        <div className="getquotebg pt--40 pb--40">
+          <Container>
+            <GetQuote />
+          </Container>
+        </div>
       </main>
-      <Footer/>
+      <Footer />
     </Fragment>
   );
 };
