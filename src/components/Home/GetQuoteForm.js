6a9737c1bc767d6398eadaea0f2b2f10b07fc1d8
aon <aon094944@gmail.com>
2023-02-03 18:26:43 +0500

    home page updated

--- src/components/Home/GetQuoteForm.js
+++ src/components/Home/GetQuoteForm.js
@@ -2,7 +2,16 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import { FaCalendar, FaCar, FaCircle, FaClock, FaLuggageCart, FaPeopleCarry, FaUser, FaUserFriends } from "react-icons/fa";
+import {
+  FaCalendar,
+  FaCar,
+  FaCircle,
+  FaClock,
+  FaLuggageCart,
+  FaPeopleCarry,
+  FaUser,
+  FaUserFriends,
+} from "react-icons/fa";
 
 function GetQuoteForm() {
   return (
@@ -61,8 +70,8 @@ function GetQuoteForm() {
               <Col>
                 <Form.Label className="input-from__label">Date</Form.Label>
                 <Form.Control
-                  type="text"
-                  className="input-from__input"
+                  type="date"
+                  className="input-from__input input-from__input-date"
                   placeholder="Select Date"
                 />
               </Col>
@@ -82,11 +91,16 @@ function GetQuoteForm() {
                 <Form.Label className="input-from__label">
                   No. of Hours
                 </Form.Label>
-                <Form.Control
-                  type="text"
+
+                <Form.Select
                   className="input-from__input"
-                  placeholder="Select Date"
-                />
+                  aria-label="Default select example"
+                >
+                  <option>Select hours</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </Form.Select>
               </Col>
             </Row>
           </div>
