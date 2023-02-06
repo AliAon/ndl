c252078593ba15aaefde315661c62291b619b734
aon <aon094944@gmail.com>
2023-02-06 20:00:13 +0500

    Home page updated

--- src/components/Home/CtaOne.js
+++ src/components/Home/CtaOne.js
@@ -2,23 +2,22 @@ import { Row, Col, Container } from "react-bootstrap";
 const CtaOne = () => {
   return (
     <div className="section__cta pt--30 pb--30">
-      
-      <Row>
-        <Col lg={5}>
-          <div className="section__cta__news-letter">
-            <h3 className="section__cta__news-letter__title">Join our newsletter now!</h3>
-            <p className="section__cta__news-letter__description">Register now and get our latest updates and promos.</p>
-            <Row>
-              <Col lg={8}>
-              </Col>
-              <Col></Col>
-            </Row>
-          </div>
-         
-        </Col>
-        <Col lg={7}></Col>
-      </Row>
-
+      <Container>
+        <Row>
+          <Col lg={5}>
+            <div className="section__cta__news-letter">
+              <h3 className="section__cta__news-letter__title">
+                We love to help you maximize your e-commerce.
+              </h3>
+              <Row>
+                <Col lg={8}></Col>
+                <Col></Col>
+              </Row>
+            </div>
+          </Col>
+          <Col lg={7}></Col>
+        </Row>
+      </Container>
     </div>
   );
 };
