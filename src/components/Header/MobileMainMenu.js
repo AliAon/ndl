57230271c6325aa4f3c660dfaa400b5fb32ffa94
aon <aon094944@gmail.com>
2023-02-07 15:28:41 +0500

    home page updated

--- src/components/Header/MobileMainMenu.js
+++ src/components/Header/MobileMainMenu.js
@@ -7,10 +7,10 @@ const MobileMainMenu = () => {
   return (
     <ul className="primary-nav-link-mobile">
       <li><Link to="/" className="primary-nav-link-mobile__a">Home</Link></li>
-      <li><Link to="/about" className="primary-nav-link-mobile__a">About</Link></li>
-      <li><Link to="/products" className="primary-nav-link-mobile__a">Products</Link></li>
-      <li><Link to="/product-details" className="primary-nav-link-mobile__a">Product Detail</Link></li>
-      <li><Link to="/product-categories" className="primary-nav-link-mobile__a">Categoires</Link></li>
+      <li><Link to="/about" className="primary-nav-link-mobile__a">About Us</Link></li>
+      <li><Link to="/our-fleet" className="primary-nav-link-mobile__a">Our Fleet</Link></li>
+      <li><Link to="/services" className="primary-nav-link-mobile__a">Services</Link></li>
+      <li><Link to="/blog" className="primary-nav-link-mobile__a">Blog</Link></li>
       <li><Link to="/contact" className="primary-nav-link-mobile__a">Contact</Link></li>
     </ul>
   );
