07cc86b9e8acf7857da814e6ede1582a73fa01bc
aon <aon094944@gmail.com>
2023-02-08 15:45:07 +0500

    HomePage Updated

--- src/components/Home/ProductTab/ProductItem.js
+++ src/components/Home/ProductTab/ProductItem.js
@@ -5,8 +5,8 @@ function ProductItem(props) {
     const {title,description,imagename}=props
   return (
     <Container className="mt--40">
-      <Row className=" row__productitem">
-        <Col lg={4} >
+      <Row className=" row__productitem align-middle">
+        <Col lg={3} >
             
           <img src={`./images/${imagename}`} className="productTabItem__product-item-details__img" />
         </Col>
@@ -21,12 +21,12 @@ function ProductItem(props) {
             <p className="productTabItem__product-item-details__description section-extend-your-shop__des">
              {description}
             </p>
-            <Row>
-              <Col lg={6} xs={3}>
+            <Row className="align-baseline">
+              <Col lg={6} xs={12}>
                 <h4 className="productTabItem__product-item-details__subtitle">Platform</h4>
               </Col>
-              <Col lg={6} xs={9}>
-                <p className="section-extend-your-shop__des text-underline">Prestashop 1.6 Prestashop 1.7</p>
+              <Col lg={6} xs={12}>
+                <p className="section-extend-your-shop__des productTabItem__product-item-details__links  text-underline">Prestashop 1.6 Prestashop 1.7</p>
               </Col>
             </Row>
           </div>
